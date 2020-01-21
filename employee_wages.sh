07229d8d86faa35888e6012412dcc5fa0008c3bb
mangesh <mlatekar@gmail.com>
2020-01-21 17:27:25 +0530

    ADDED: Emplyoee monthly wages

--- employee_wages.sh
+++ employee_wages.sh
@@ -4,20 +4,28 @@ FULL_DAY_HR=8
 HALF_DAY_HR=4
 isfulltime=1
 isparttime=2
+WORKING_DAY=20
+
+for (( i=1; i<=$WORKING_DAY; i++))
+do
 attendance=$((RANDOM%3))
 		case $attendance in	
-			$isfulltime)
+         $isfulltime)
 				echo "Emplyoee present with full time"
-				dailywages=$(($WAGE_PER_HR*$FULL_DAY_HR))
+				dailywages=$(($dailywages+($WAGE_PER_HR*$FULL_DAY_HR)))
 				echo "wages $dailywages "
 				;;
 	
-         $isparttime)   
+        	$isparttime)   
 	      	echo "Emplyoee present half day "
-				dailywages=$(($WAGE_PER_HR*$HALF_DAY_HR))
+				dailywages=$(($dailywages+($WAGE_PER_HR*$HALF_DAY_HR)))
 				echo "Wages $dailywages"
 				;;
          *)
 				echo "Emplyoee is absent"
+				((dailywages+=0))
 				;;
 esac
+done
+echo "Monthly wages : " $dailywages
+
