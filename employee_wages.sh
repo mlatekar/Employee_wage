dccbc4a55a2acf325ba0436bdcec7358c148d26c
mangesh <mlatekar@gmail.com>
2020-01-21 15:03:02 +0530

    ADDED :daily wage calculation

--- employee_wages.sh
+++ employee_wages.sh
@@ -1,9 +1,13 @@
 echo " welcome to  Employe wage"
+wage_per_hr=20
+full_day_hr=8
 
 attandance=$((RANDOM%2))
 if [[ $attandance -eq 0 ]]
 then
 echo "emplyoee present"
+dailywages=$(($wage_per_hr*$full_day_hr))
+echo "wages $dailywages "
 else 
 echo "absent"
 fi
