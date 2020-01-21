fbb0303ecb98f8089fe0332565d0ab0f1768b616
mangesh <mlatekar@gmail.com>
2020-01-21 18:24:43 +0530

    MODIFIED:wages_100hrs and 20days use while()

--- employee_wages.sh
+++ employee_wages.sh
@@ -1,12 +1,14 @@
+#! /bin/bash -x
 echo "Welcome to  Employe wage"
 WAGE_PER_HR=20
 FULL_DAY_HR=8
 HALF_DAY_HR=4
 isfulltime=1
 isparttime=2
-WORKING_DAY=20
+working_day=0
+working_hr=0
 
-for (( i=1; i<=$WORKING_DAY; i++))
+while [[ $working_day -le 20 && $working_hr -le 100 ]]
 do
 attendance=$((RANDOM%3))
 		case $attendance in	
@@ -14,13 +16,16 @@ attendance=$((RANDOM%3))
 				echo "Emplyoee present with full time"
 				dailywages=$(($dailywages+($WAGE_PER_HR*$FULL_DAY_HR)))
 				echo "wages $dailywages "
+			   ((working_day++))
+            ((working_hr+=8))
 				;;
-	
         	$isparttime)   
 	      	echo "Emplyoee present half day "
-				dailywages=$(($dailywages+($WAGE_PER_HR*$HALF_DAY_HR)))
+  			  	dailywages=$(($dailywages+($WAGE_PER_HR*$HALF_DAY_HR)))
 				echo "Wages $dailywages"
-				;;
+			   ((working_day++))
+            ((working_hr+=4))
+         	;;
          *)
 				echo "Emplyoee is absent"
 				((dailywages+=0))
@@ -28,4 +33,5 @@ attendance=$((RANDOM%3))
 esac
 done
 echo "Monthly wages : " $dailywages
-
+echo "Total working hours : " $working_hr
+echo "Total working day : " $working_day
