6f0938fefaf42941fbd17aed3f7f071b1b281359
mangesh <mlatekar@gmail.com>
2020-01-21 16:56:53 +0530

    ADDED:wages case statment

--- employee_wages.sh
+++ employee_wages.sh
@@ -2,17 +2,22 @@ echo "Welcome to  Employe wage"
 WAGE_PER_HR=20
 FULL_DAY_HR=8
 HALF_DAY_HR=4
+isfulltime=1
+isparttime=2
 attendance=$((RANDOM%3))
-	if [[ $attendance -eq 1 ]]
-		then
-			echo "Emplyoee present with full time"
-			dailywages=$(($WAGE_PER_HR*$FULL_DAY_HR))
-			echo "wages $dailywages "
-	elif [[ $attendance -eq 2 ]]
-			then
-         echo "Emplyoee present half day "
-			dailywages=$(($WAGE_PER_HR*$HALF_DAY_HR))
-			echo "Wages $dailywages"
-	else 
-			echo "Emplyoee is absent"
-	fi
+		case $attendance in	
+			$isfulltime)
+				echo "Emplyoee present with full time"
+				dailywages=$(($WAGE_PER_HR*$FULL_DAY_HR))
+				echo "wages $dailywages "
+				;;
+	
+         $isparttime)   
+	      	echo "Emplyoee present half day "
+				dailywages=$(($WAGE_PER_HR*$HALF_DAY_HR))
+				echo "Wages $dailywages"
+				;;
+         *)
+				echo "Emplyoee is absent"
+				;;
+esac
