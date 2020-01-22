0ff652e4d1618c678694680bb0a65c748ac046c1
mangesh <mlatekar@gmail.com>
2020-01-22 10:32:08 +0530

    REFACTORED:create function add new variable remove unwanted variable

--- employee_wages.sh
+++ employee_wages.sh
@@ -1,37 +1,32 @@
-#! /bin/bash -x
+#!/bin/bash -x
 echo "Welcome to  Employe wage"
 WAGE_PER_HR=20
-FULL_DAY_HR=8
-HALF_DAY_HR=4
 isfulltime=1
 isparttime=2
-working_day=0
 working_hr=0
-
-while [[ $working_day -le 20 && $working_hr -le 100 ]]
-do
-attendance=$((RANDOM%3))
-		case $attendance in	
+totalWorkingHours=0
+totalWorkingDays=0
+function getWorkingHours(){		
+case $1 in	
          $isfulltime)
-				echo "Emplyoee present with full time"
-				dailywages=$(($dailywages+($WAGE_PER_HR*$FULL_DAY_HR)))
-				echo "wages $dailywages "
-			   ((working_day++))
-            ((working_hr+=8))
+			   ((working_hr=8))
 				;;
         	$isparttime)   
-	      	echo "Emplyoee present half day "
-  			  	dailywages=$(($dailywages+($WAGE_PER_HR*$HALF_DAY_HR)))
-				echo "Wages $dailywages"
-			   ((working_day++))
-            ((working_hr+=4))
+	         ((working_hr=4))
          	;;
          *)
-				echo "Emplyoee is absent"
-				((dailywages+=0))
+				((dailywages=0))
 				;;
-esac
+		esac
+	#	echo $working_hr
+}
+while [[ $totalWorkingDays -le 20 && $totalWorkingHours -le 100 ]]
+do
+		((totalWorkingDays++))
+ 		getWorkingHours $((RANDOM%3)) 
+ 		totalWorkingHours=$(($totalWorkingHours+$working_hr))
+
 done
-echo "Monthly wages : " $dailywages
-echo "Total working hours : " $working_hr
-echo "Total working day : " $working_day
+salary=$(($totalWorkingHours*$WAGE_PER_HR))
+echo  "Total working hours : " $totalWorkingHours
+echo  "Total monthly salary : "$salary
